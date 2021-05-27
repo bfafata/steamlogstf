6b969c54883519887110f317da61cc32e72011e4
bfafata <70115786+bfafata@users.noreply.github.com>
2021-05-27 15:41:08 -0400

    asdfasdfasdf

---
+++ jsstuff/variables.js
@@ -0,0 +1,4 @@
+let name = "john"
+let big = 'larry'
+let score = 102
+console.log(name)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pathlib
 
 myfile = pathlib.Path('myfile.txt')
+pathlib.Path('')
 myfile.touch(exist_ok=True)
 f = open(myfile)
